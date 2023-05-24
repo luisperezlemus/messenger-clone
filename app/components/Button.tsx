1fbaccf3b1d740abc8fd01401c271335c7b9ff5f
luisperezlemus <lperezle@asu.edu>
2023-05-24 08:19:01 -0700

    Allow user to delete a conversation

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -34,8 +34,8 @@ const Button: React.FC<ButtonProps> = ({
                 py-2
                 text-sm
                 font-semibold
-                focus-visible: outline
-                focus-visible: outline-2
+                focus-visible:outline
+                focus-visible:outline-2
                 focus-visible:outline-offset-2
             `,
             disabled && 'opacity-50 cursor-default',
